7e4109db34b9ce92bbbb68186eb2b660bdd3f7f4
Chris Sterling <csterling@pivotal.io>
2017-06-12 10:15:14 -0700

    Upgraded SCS starters/connectors dependency to 1.5.0. Upgraded Spring Cloud dependency to Dalston.SR1. Upgraded Spring Boot dependency to 1.5.2.RELEASE. Added maven wrapper to ease packaging of app. Changed SpringApplicationConfiguration import to SpringBootTest based on changes to autoconfiguration of tests done in Spring Boot 1.5.x.

--- fortune-teller-ui/src/test/java/io/spring/cloud/samples/fortuneteller/ui/services/fortunes/FortuneServicePactTest.java
+++ fortune-teller-ui/src/test/java/io/spring/cloud/samples/fortuneteller/ui/services/fortunes/FortuneServicePactTest.java
@@ -8,7 +8,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -21,7 +21,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringBootTest
 @WebAppConfiguration
 @ActiveProfiles({"pact"})
 public class FortuneServicePactTest {
