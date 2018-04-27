ead2df79daf770a4be92223d66e7d3c4fa446b3b
Roy Clarkson <rclarkson@pivotal.io>
2018-04-26 20:56:45 -0400

    Update pws deployment script to use new trial plans and rename services

--- scripts/pws-create-services.sh
+++ scripts/pws-create-services.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cf create-service cleardb spark fortunes-db
-cf create-service p-config-server standard config-server -c '{"git": { "uri": "https://github.com/spring-cloud-services-samples/fortune-teller", "searchPaths": "configuration" } }'
-cf create-service p-service-registry standard service-registry
-cf create-service p-circuit-breaker-dashboard standard circuit-breaker
+cf create-service p-config-server trial fortunes-config-server -c '{"git": { "uri": "https://github.com/spring-cloud-services-samples/fortune-teller", "searchPaths": "configuration" } }'
+cf create-service p-service-registry trial fortunes-service-registry
+cf create-service p-circuit-breaker-dashboard trial fortunes-circuit-breaker-dashboard
